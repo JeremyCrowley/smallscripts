8d470b28da71f3300f34721a3e76240226f8a753
Jeremy Crowley <jcrowley@ucsc.edu>
2017-04-27 12:50:18 -0700

    can scrape title and price. Location is a bit iffy

--- webscraper/carfinder.py
+++ webscraper/carfinder.py
@@ -10,13 +10,25 @@ def main():
 	toyota = requests.get(toyhtml)
 
 	tree = html.fromstring(toyota.content)
+	cars = tree.xpath('//*[@class="result-title hdrlnk"]/text()')
+	pricedoubled = tree.xpath('//span[@class="result-price"]/text()')
+	#location = tree.xpath('//*[@class="result-hood"]/text()')
 
-	cars = tree.xpath('//a[@class="results-title hdrlnk"]/text()')
+	i = 0
+	price = []
+	for p in pricedoubled:
+		if i%2 == 0:
+			price.append(p)
 
-	price = tree.xpath('//span[@class="result-price"]/text()')
+		i = i + 1
 
-	print(cars)
-	print(price)
+	print(cars[:10])
+	print(price[:10])
+	#print(location)
+
+	print(len(cars))
+	print(len(price))
+	print(len(location))
 
 
 if __name__ == '__main__':

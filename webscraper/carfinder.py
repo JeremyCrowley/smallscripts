cf037f36c039cc6cfb4b8406ea37a8cda34203c5
Jeremy Crowley <jcrowley@ucsc.edu>
2017-04-27 22:05:39 -0700

    finds all titles and prices correctly

--- webscraper/carfinder.py
+++ webscraper/carfinder.py
@@ -2,34 +2,34 @@
 
 import requests
 from lxml import html
+from email.mime.text import MIMEText
+import smtplib
 
 
-toyhtml = "https://sfbay.craigslist.org/search/nby/sss?query=toyota&sort=rel&max_price=7000&min_auto_year=2007&auto_title_status=1&auto_transmission=2"
+toyhtml = "https://sfbay.craigslist.org/search/nby/sss?sort=date&auto_title_status=1&auto_transmission=2&max_price=7000&min_auto_year=2007&query=toyota"
 
 def main():
+
 	toyota = requests.get(toyhtml)
-
 	tree = html.fromstring(toyota.content)
+
 	cars = tree.xpath('//*[@class="result-title hdrlnk"]/text()')
-	pricedoubled = tree.xpath('//span[@class="result-price"]/text()')
-	#location = tree.xpath('//*[@class="result-hood"]/text()')
+	price = tree.xpath('//span[@class="result-meta"]//span[@class="result-price"]/text()')
+	
+	#link = tree.xpath('//*[contains(/text(),"/nby/ctd")]')
 
-	i = 0
-	price = []
-	for p in pricedoubled:
-		if i%2 == 0:
-			price.append(p)
+	#print link
 
-		i = i + 1
 
-	print(cars[:10])
-	print(price[:10])
-	#print(location)
 
-	print(len(cars))
-	print(len(price))
-	print(len(location))
 
+	for i in range(0,len(price)):
+
+		if("camry" in cars[i].encode('utf-8').lower() and int(price[i].encode('utf-8')[1:]) < 6000):
+			print(cars[i].encode('utf-8') + " --> " + price[i].encode('utf-8'))
+
+
+	
 
 if __name__ == '__main__':
 	main()

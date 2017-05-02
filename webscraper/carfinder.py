0cbd2925177a46d1caf957c3778b564d76b42bfd
Jeremy Crowley <jcrowley@ucsc.edu>
2017-05-02 00:45:33 -0700

    added cl input for params in search

--- webscraper/carfinder.py
+++ webscraper/carfinder.py
@@ -1,44 +1,44 @@
 
-
+import sys
 import requests
 from lxml import html
 import bs4
-
 import smtplib
 from email.mime.text import MIMEText
 
 
 
-#def message(title, price, url):
-
-
-
-
-
-
 def main():
 
-	toyhtml = "https://sfbay.craigslist.org/search/nby/sss?sort=date&auto_title_status=1&auto_transmission=2&max_price=7000&min_auto_year=2007&query=toyota"
+	for arg in sys.argv:
+		if arg.isdigit():
+			if int(arg) < 2020:
+				minyear = arg
+			else:
+				maxprice = arg
+		else:
+			search = arg
 
-	toyota = requests.get(toyhtml)
+	link = "https://sfbay.craigslist.org/search/nby/sss?sort=date&auto_title_status=1&auto_transmission=2&max_price=" + str(maxprice) + "&min_auto_year=" + str(minyear) + "&query=" + str(search)
+	
+	handle = requests.get(link)
 
 	hyperlink = []
 	
-	soup = bs4.BeautifulSoup(toyota.content, "lxml")
+	soup = bs4.BeautifulSoup(handle.content, "lxml")
 	for link in soup.findAll("a"):
 		if(".html" in link.get("href")):
 			if(link.get("href") not in hyperlink):
 				hyperlink.append(link.get("href"))
 
-	tree = html.fromstring(toyota.content)
+	tree = html.fromstring(handle.content)
 
 	cars = tree.xpath('//*[@class="result-title hdrlnk"]/text()')
 	price = tree.xpath('//span[@class="result-meta"]//span[@class="result-price"]/text()')
 	
 	for i in range(0,len(price)):
 
-		if("camry" in cars[i].encode('utf-8').lower() and int(price[i].encode('utf-8')[1:]) < 7000):
-			print(cars[i].encode('utf-8') + " --> " + price[i].encode('utf-8'))
+	 	print(cars[i].encode('utf-8') + " --> " + price[i].encode('utf-8'))
 	
 
 	
